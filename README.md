2ada7d6c684e90f77ac0a6272500e3eb0a708748
fedepaj <fedepaj@github.com>
2021-04-19 15:54:31 +0200

    Rebased code

--- README.md
+++ README.md
@@ -1,120 +1,21 @@
 # tappo
-Is a safety oriented smart lid for plastic tanks.
+Is a safety oriented smart cap tat senses liquid level.
 
-The device is placed on the neck of the tank and measures fill percentage to inform the user when it has reached the desired level and warn the user the user tor stop the flow.
+It's build manly for plastic tanks as are the ones used for axample in the following senarios:
+- My father, who need to refill acquarium's wather, and need to shut the distilled water valve before the tank is full.
+- Waste water produced by air conditioning system is stored in plastic tanks that need to be periodically emptyed.
 
-## Ultrasonic range finder sr04
-When the lid is on Is used to determine the distance between the water and the neck of the tank. 
+When the device is inserted, it senses it and starts measuring the distance between the liquid and itself returning the fill percentage of the container based on the fist measure that it makes.
+When a percentage near 100% is reached the device will alert the user via it's actuators.
 
-The device gets a reference masure of the distance between the sensor and the level of the bottom of the tank or the level of already present fluid.
+## Hardware
+To sense the insertion a switch is used, an ultrasonic sensor is used to measure the liquid quantity and to alert the user a led and a buzzer are used. 
+To see how the hardware is put togheder please refer to [this file]().
 
-The difference between the reference vaule and the subsequent distance's samples defines the fill percentage that will be sended via the `measures` topic to the broker.
+### Nucleo board
 
-## The switch
-The role of the switch is to decect when the lid has been placed on the neck of the tank and could be any sort of switch.
-
-The switch chosen is [this one](http://smparts.com/product_info.php?cPath=2_602&products_id=6689),a lever switch with a little wheel on top of it.
-Is placed in the circuit like [this](https://killerrobotics.files.wordpress.com/2015/09/lever-switch_bb.png).
-
-The switch it raises an interruppt that publishes the activeness of the lid to the `active` topic and let the device stop measuring the fill percentage.
-
-## Case
-Is the one that holds the sensors together and makes it a perfect lid for a 43mm 30L plastic tank that will be filled with 15mm tubing.
-
-## Piezo buzzer
-Informs the user that the level of the water has reached the desired level with an acoustic signal.
-
-## Led
-Is surely a visual cue but could really be switched by a relay.
-
-## Network setup (NUCLEO side)
-
-For me networking didn't work out of the box so I've added two functions
-looking at the ones in RIOT's [sc_gnrc_netif.c](https://github.com/RIOT-OS/RIOT/blob/2453b68249d09ff680f3ea85343e26851d00da67/sys/shell/commands/sc_gnrc_netif.c) to mimic the behavior of the `ifconfig` command in [emcute_mqttsn](https://github.com/RIOT-OS/RIOT/tree/master/examples/emcute_mqttsn) (that was the only way to connect to broker).
-
-First we define:
-```c
-#define _IPV6_DEFAULT_PREFIX_LEN        (64U)
-```
-Then we have:
-```c
-static uint8_t get_prefix_len(char *addr)
-{
-    int prefix_len = ipv6_addr_split_int(addr, '/', _IPV6_DEFAULT_PREFIX_LEN);
-
-    if (prefix_len < 1) {
-        prefix_len = _IPV6_DEFAULT_PREFIX_LEN;
-    }
-
-    return prefix_len;
-}
-
-
-
-static int netif_add(char *iface_name,char *addr_str)
-{
-
-    netif_t *iface = netif_get_by_name(iface_name);
-        if (!iface) {
-            puts("error: invalid interface given");
-            return 1;
-        }
-    enum {
-        _UNICAST = 0,
-        _ANYCAST
-    } type = _UNICAST;
-    
-    ipv6_addr_t addr;
-    uint16_t flags = GNRC_NETIF_IPV6_ADDRS_FLAGS_STATE_VALID;
-    uint8_t prefix_len;
-
-
-    prefix_len = get_prefix_len(addr_str);
-
-    if (ipv6_addr_from_str(&addr, addr_str) == NULL) {
-        puts("error: unable to parse IPv6 address.");
-        return 1;
-    }
-
-    if (ipv6_addr_is_multicast(&addr)) {
-        if (netif_set_opt(iface, NETOPT_IPV6_GROUP, 0, &addr,
-                          sizeof(addr)) < 0) {
-            printf("error: unable to join IPv6 multicast group\n");
-            return 1;
-        }
-    }
-    else {
-        if (type == _ANYCAST) {
-            flags |= GNRC_NETIF_IPV6_ADDRS_FLAGS_ANYCAST;
-        }
-        flags |= (prefix_len << 8U);
-        if (netif_set_opt(iface, NETOPT_IPV6_ADDR, flags, &addr,
-                          sizeof(addr)) < 0) {
-            printf("error: unable to add IPv6 address\n");
-            return 1;
-        }
-    }
-
-    printf("success: added %s/%d to interface ", addr_str, prefix_len);
-    printf("\n");
-
-    return 0;
-
-}
-```
-Finally we can call `netif_add` in `main` as:
-```c
-netif_add("4","fec0:affe::99");
-```
-Last but not least we need to add the following in `Makefile.ethos.conf` to let RIOT add a third entry tho the network interface:
-```make
-CFLAGS += -DCONFIG_GNRC_NETIF_IPV6_ADDRS_NUMOF=3
-```
-and in `Makefile`:
-```make
-SERVER_ADDR = fec0:affe::1
-```
-## Networking (Linux side)
+## On linux
+### Make the nucleo network work
 To make this work basically following [emcute_mqttsn](https://github.com/RIOT-OS/RIOT/tree/master/examples/emcute_mqttsn#setting-up-riot-native), so:
 
 ```sh 
@@ -122,7 +23,7 @@ sudo ./RIOT/dist/tools/tapsetup/tapsetup
 sudo ip a a fec0:affe::1/64 dev tapbr0
 ```
 
-## Run the MQTT-SN broker
+### Run the MQTT-SN broker
 
 This is pretty straight forward following [emcute_mqttsn](https://github.com/RIOT-OS/RIOT/tree/master/examples/emcute_mqttsn#setting-up-a-broker) and caring to add at the bottom of the conf file the part relative to the transparent bridge.
 ```
@@ -133,7 +34,7 @@ connection local_bridge_to_mosquitto
   topic commands in
 ```
 
-## Bridge between MQTT-SN and MQTT
+### Bridge between MQTT-SN and MQTT
 We will use mosquitto like [here](https://aws.amazon.com/it/blogs/iot/how-to-bridge-mosquitto-mqtt-broker-to-aws-iot/).
 To begin we need the following from Amazon:
 * root certificate 
@@ -141,3 +42,13 @@ To begin we need the following from Amazon:
 * client private key
 Now we need to specify in a mosquitto config file the adress of the AWT IoT core service and the topics, and their specific diretctions, that we want to bridge.
 To then run mosquitto with the config file in the current directory, after copying the AWS certificates `/etc/mosquitto/certs/`, as stated in the link above, we can also stop the service and issue the command with flag `-c` followed by the name of the file. 
+
+## AWS
+
+### IoT Core
+
+### Gateway Api
+
+## Dashboard
+
+## Final results
--- nucleo/main.c
+++ nucleo/main.c
@@ -1,43 +1,154 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <time.h>
 
-#include "timex.h"
-#include "shell.h"
+#include "xtimer.h"
+#include "periph/gpio.h"
+#include "periph/pwm.h"
+
+//Ultrasonic distance sensor
+#include "srf04.h"
+#include "srf04_params.h"
+#include "fmt.h"
+
+//Networking
 #include "msg.h"
 #include "net/emcute.h"
 #include "net/ipv6/addr.h"
-#include "xtimer.h"
-#include "srf04.h"
-#include "srf04_params.h"
 
-#ifndef EMCUTE_ID
-#define EMCUTE_ID           ("gertrud")  //Putting long names currently may cause buffer overflow
-#endif
-#define EMCUTE_PRIO         (THREAD_PRIORITY_MAIN - 1)
+//PWM
+#define PWM_FREQ (494U)
+#define PWM_RES (255U)
 
+//Netwworking
 #define _IPV6_DEFAULT_PREFIX_LEN        (64U)
 
+//Emcute
+#define EMCUTE_PRIO         (THREAD_PRIORITY_MAIN - 1)
 #define NUMOFSUBS           (16U)
 #define TOPIC_MAXLEN        (64U)
 
-static char stack[THREAD_STACKSIZE_DEFAULT];
-
 static emcute_sub_t subscriptions[NUMOFSUBS];
-static char topics[NUMOFSUBS][TOPIC_MAXLEN];
+static char stack[THREAD_STACKSIZE_MAIN];
 
-gpio_t trigger_pin = GPIO_PIN(PORT_A, 10); //D2
-gpio_t echo_pin = GPIO_PIN(PORT_B, 3); //D3
-gpio_t alert_pin = GPIO_PIN(PORT_B, 5); //D4
-gpio_t led_pin = GPIO_PIN(PORT_B,4); //D5
-gpio_t operative_pin = GPIO_PIN(PORT_A, 2); //D1
+gpio_t trigger_gpio = GPIO_PIN(PORT_A, 10); //D2
+//D3 PWM
+gpio_t echo_gpio = GPIO_PIN(PORT_B, 5); //D4
+gpio_t led_gpio = GPIO_PIN(PORT_B, 4); //D5
+//D6 PWM
+gpio_t sw_gpio = GPIO_PIN(PORT_A,8); //D7
 
-char stmp_str[10]; 
-int perc=0;
-int onTop=0;
-int isReferenceMeasure=0;
-int margin=50; //Default margin
+typedef enum state {
+    STANDBY, REFERENCE, MEASURING, CLEAN, ALERT
+}state;
+
+state currState = STANDBY;
+typedef enum alertstate {
+    ALERTED, NOT_ALERTED
+}alertstate;
+
+alertstate currAlertS  = NOT_ALERTED; 
+/*Prototypes*/
+
+//Network prototypes
+static uint8_t get_prefix_len(char *addr);
+static int netif_add(char *iface_name,char *addr_str);
+
+//MQTT-SN emcute prototypes
+int setup_mqtt(void);
+static void *emcute_thread(void *arg);
+static int pub(char* topic, const char* data, int qos);
+static void on_pub_buzz(const emcute_topic_t *topic, void *data, size_t len);
+static void on_pub_led(const emcute_topic_t *topic, void *data, size_t len);
+
+
+void switch_cb(void *arg){
+    (void)arg;
+    int val = gpio_read(sw_gpio);
+    if(val){
+        currState=REFERENCE;
+        puts("ON");
+    }else {
+        currState=CLEAN;
+        puts("OFF");
+    }fflush(stdin);
+}
+
+int main(void)
+{
+
+    int dist=0;
+    int ref=0;
+    int perc=0;
+    char buffer[15];
+    srf04_t dev;
+    srf04_params_t my_params;
+    my_params.trigger = trigger_gpio;
+    my_params.echo = echo_gpio;
+    
+    if(srf04_init(&dev, &my_params) != SRF04_OK){
+       puts("Failed to setup SRF04");
+    }
+    setup_mqtt();
+    pwm_init(PWM_DEV(0),PWM_LEFT,PWM_FREQ,PWM_RES); //USING D3(BUZZ) AND D6(UNUSABLE)
+    pwm_set(PWM_DEV(0),1,100);
+    pwm_poweroff(PWM_DEV(0));
+    gpio_init(led_gpio,GPIO_OUT);
+    gpio_init_int(sw_gpio, GPIO_IN, GPIO_BOTH, &switch_cb, NULL);
+    while(1){
+        switch(currState){
+            case REFERENCE:
+                pub(MQTT_TOPIC_ACT, "{\"setOn\":\"1\"}", 0);
+                ref=srf04_get_distance(&dev);
+                puts("IN MAIN STATE REFERENCE");
+                if(ref>0)
+                    currState=MEASURING;
+                break;
+            case MEASURING:
+                puts("IN MAIN STATE MEASURING");
+                dist=srf04_get_distance(&dev);
+                if(dist>0){
+                    perc=(int)((1.0-((double)dist/(double)ref))*100);
+                    sprintf(buffer,"{\"perc\":\"%d\"}",perc);
+                    pub(MQTT_TOPIC_MEAS, buffer, 0);
+                    puts(buffer);
+                    for(int i=0;i<15;i++)
+                        buffer[i]=0;
+                    if(perc>=70){
+                        currState=ALERT;
+                    }
+                    xtimer_sleep(5);
+                }else xtimer_sleep(1);
+                break;
+            case CLEAN:
+                //This case is used to clean stuff
+                ref=0;
+                perc=0;
+                dist=0;
+                pub(MQTT_TOPIC_ACT, "{\"setOn\":\"0\"}", 0);
+                pub(MQTT_TOPIC_LED, "{\"setOn\":\"0\"}", 0);
+                pub(MQTT_TOPIC_BUZZ, "{\"setOn\":\"0\"}", 0);
+                currState=STANDBY;
+                currAlertS=NOT_ALERTED;
+                break;
+            case STANDBY:
+                puts("IN MAIN STATE STANDBY");
+                xtimer_sleep(5);
+                break;
+            case ALERT:
+                puts("IN MAIN STATE ALERT");
+                if(currAlertS==NOT_ALERTED){
+                    pub(MQTT_TOPIC_LED, "{\"setOn\":\"1\"}", 0);
+                    pub(MQTT_TOPIC_BUZZ, "{\"setOn\":\"1\"}", 0);
+                    currAlertS=ALERTED;
+                }
+                currState=MEASURING;
+                break;
+        }
+    }
+
+}
+
 static void *emcute_thread(void *arg)
 {
     (void)arg;
@@ -45,6 +156,124 @@ static void *emcute_thread(void *arg)
     return NULL;    /* should never be reached */
 }
 
+int setup_mqtt(void)
+{
+    /* initialize our subscription buffers */
+    memset(subscriptions, 0, (NUMOFSUBS * sizeof(emcute_sub_t)));
+
+    /* start the emcute thread */
+    thread_create(stack, sizeof(stack), EMCUTE_PRIO, 0, emcute_thread, NULL, "emcute");
+    //Adding address to network interface
+    netif_add("4","fec0:affe::99");
+    // connect to MQTT-SN broker
+    printf("Connecting to MQTT-SN broker %s port %d.\n", SERVER_ADDR, SERVER_PORT);
+
+    sock_udp_ep_t gw = {
+        .family = AF_INET6,
+        .port = SERVER_PORT
+    };
+    
+    /* parse address */
+    if (ipv6_addr_from_str((ipv6_addr_t *)&gw.addr.ipv6, SERVER_ADDR) == NULL) {
+        printf("error parsing IPv6 address\n");
+        return 1;
+    }
+
+    if (emcute_con(&gw, true, NULL, NULL, 0, 0) != EMCUTE_OK) {
+        printf("error: unable to connect to [%s]:%i\n", SERVER_ADDR, (int)gw.port);
+        return 1;
+    } else printf("Successfully connected to gateway at [%s]:%i\n", SERVER_ADDR, (int)gw.port);
+
+    // setup subscription to topic
+    
+    unsigned flags = EMCUTE_QOS_0;
+    subscriptions[0].cb = on_pub_led;
+    subscriptions[1].cb = on_pub_buzz;
+
+    subscriptions[0].topic.name = MQTT_TOPIC_LED;
+    subscriptions[1].topic.name = MQTT_TOPIC_BUZZ;
+
+    if (emcute_sub(&subscriptions[0], flags) != EMCUTE_OK) {
+        printf("error: unable to subscribe to %s\n", MQTT_TOPIC_LED);
+        return 1;
+    }
+
+    if (emcute_sub(&subscriptions[1], flags) != EMCUTE_OK) {
+        printf("error: unable to subscribe to %s\n", MQTT_TOPIC_BUZZ);
+        return 1;
+    }
+    printf("Now subscribed to %s\n", MQTT_TOPIC_LED);
+    printf("Now subscribed to %s\n", MQTT_TOPIC_BUZZ);
+    
+    return 0;
+}
+
+static int pub(char* topic, const char* data, int qos){
+    emcute_topic_t t;
+    unsigned flags = EMCUTE_QOS_0;
+
+    switch(qos){
+        case 1:
+            flags |= EMCUTE_QOS_1;
+            break;
+        case 2:
+            flags |= EMCUTE_QOS_2;
+            break;
+        default:
+            flags |= EMCUTE_QOS_0;
+            break;
+
+    }
+
+    t.name = topic;
+    if(emcute_reg(&t) != EMCUTE_OK){
+        printf("PUB ERROR: Unable to obtain Topic ID, %s",topic);
+        return 1;
+    }
+    if(emcute_pub(&t, data, strlen(data), flags) != EMCUTE_OK){
+        printf("PUB ERROR: unable to publish data to topic '%s [%i]'\n", t.name, (int)t.id);
+        return 1;
+    }
+
+    printf("PUB SUCCESS: Published %s on topic %s\n", data, topic);
+    return 0;
+}
+
+static void on_pub_buzz(const emcute_topic_t *topic, void *data, size_t len){
+    char *in = (char *)data;
+    printf("### got publication for topic '%s' [%i] ###\n",
+           topic->name, (int)topic->id);
+    if(strcmp(data,"{\"setOn\":\"1\"}")==0){
+        pwm_poweron(PWM_DEV(0));
+    }
+    if(strcmp(data,"{\"setOn\":\"0\"}")==0){
+        pwm_poweroff(PWM_DEV(0));
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        printf("%c", in[i]);
+    }
+    puts("");
+}
+
+static void on_pub_led(const emcute_topic_t *topic, void *data, size_t len){
+    char *in = (char *)data;
+    printf("### got publication for topic '%s' [%i] ###\n",
+           topic->name, (int)topic->id);
+    if(strcmp(data,"{\"setOn\":\"1\"}")==0){
+        gpio_set(led_gpio);
+    }
+    if(strcmp(data,"{\"setOn\":\"0\"}")==0){
+        gpio_clear(led_gpio);
+    }
+
+    for (size_t i = 0; i < len; i++) {
+        printf("%c", in[i]);
+    }
+    puts("");
+}
+
+/********* NETWORK RELATED PART *********/ 
 static uint8_t get_prefix_len(char *addr)
 {
     int prefix_len = ipv6_addr_split_int(addr, '/', _IPV6_DEFAULT_PREFIX_LEN);
@@ -106,179 +335,5 @@ static int netif_add(char *iface_name,char *addr_str)
     return 0;
 
 }
+/********* NETWORK RELATED PART END *********/ 
 
-static void on_pub(const emcute_topic_t *topic, void *data, size_t len){
-    char *in = (char *)data;
-    printf("### got publication for topic '%s' [%i] ###\n",
-           topic->name, (int)topic->id);
-    for (size_t i = 0; i < len; i++) {
-        printf("%c", in[i]);
-    }
-    puts("");
-}
-
-static int pub(char* topic, const char* data, int qos){
-    emcute_topic_t t;
-    unsigned flags = EMCUTE_QOS_0;
-
-    switch(qos){
-        case 1:
-            flags |= EMCUTE_QOS_1;
-            break;
-        case 2:
-            flags |= EMCUTE_QOS_2;
-            break;
-        default:
-            flags |= EMCUTE_QOS_0;
-            break;
-
-    }
-
-    t.name = MQTT_TOPIC_OUT;
-    if(emcute_reg(&t) != EMCUTE_OK){
-        puts("PUB ERROR: Unable to obtain Topic ID");
-        return 1;
-    }
-    if(emcute_pub(&t, data, strlen(data), flags) != EMCUTE_OK){
-        printf("PUB ERROR: unable to publish data to topic '%s [%i]'\n", t.name, (int)t.id);
-        return 1;
-    }
-
-    printf("PUB SUCCESS: Published %s on topic %s\n", data, topic);
-    return 0;
-}
-
-int measure_distance(srf04_t *dev){
-    if(srf04_read(dev) == SRF04_ERR_INVALID){
-        printf("No valid measurement is available!\n");
-        return -1;
-    }
-    int distance = srf04_get_distance(dev);
-    //printf("SRF04 distance: %d mm\n", distance);
-    return distance;
-}
-
-int setup_mqtt(void)
-{
-    /* initialize our subscription buffers */
-    memset(subscriptions, 0, (NUMOFSUBS * sizeof(emcute_sub_t)));
-
-    /* start the emcute thread */
-    thread_create(stack, sizeof(stack), EMCUTE_PRIO, 0, emcute_thread, NULL, "emcute");
-    //Adding address to network interface
-    netif_add("4","fec0:affe::99");
-    // connect to MQTT-SN broker
-    printf("Connecting to MQTT-SN broker %s port %d.\n", SERVER_ADDR, SERVER_PORT);
-
-    sock_udp_ep_t gw = {
-        .family = AF_INET6,
-        .port = SERVER_PORT
-    };
-    
-    char *message = "connected";
-    size_t len = strlen(message);
-
-    /* parse address */
-    if (ipv6_addr_from_str((ipv6_addr_t *)&gw.addr.ipv6, SERVER_ADDR) == NULL) {
-        printf("error parsing IPv6 address\n");
-        return 1;
-    }
-
-    if (emcute_con(&gw, true, MQTT_TOPIC_OUT, message, len, 0) != EMCUTE_OK) {
-        printf("error: unable to connect to [%s]:%i\n", SERVER_ADDR, (int)gw.port);
-        return 1;
-    }
-
-    printf("Successfully connected to gateway at [%s]:%i\n", SERVER_ADDR, (int)gw.port);
-
-    // setup subscription to topic
-    
-    unsigned flags = EMCUTE_QOS_0;
-    subscriptions[0].cb = on_pub;
-    strcpy(topics[0], MQTT_TOPIC_IN);
-    subscriptions[0].topic.name =MQTT_TOPIC_IN;
-
-    if (emcute_sub(&subscriptions[0], flags) != EMCUTE_OK) {
-        printf("error: unable to subscribe to %s\n", MQTT_TOPIC_IN);
-        return 1;
-    }
-
-    printf("Now subscribed to %s\n", MQTT_TOPIC_IN);
-    
-    return 0;
-}
-
-void genTimeStamp(char* stmp_str){
-    timex_t stmp = timex_set(3,3);
-    timex_to_str(stmp, stmp_str);
-}
-
-void switch_cb(void *arg){
-    puts(arg);
-    char str[40];
-    int val = gpio_read(operative_pin);
-    if(val){
-        onTop=1;
-        isReferenceMeasure=1;
-        genTimeStamp(stmp_str);
-        sprintf(str, "{\"id\":\"%s\",\"stmp\":\"%s\",\"active\":\"1\"}}",EMCUTE_ID, stmp_str);
-        pub(MQTT_TOPIC_OUT, str, 0);
-    } else {
-        onTop=0;
-        perc=0;
-        genTimeStamp(stmp_str);
-        sprintf(str, "{\"id\":\"%s\",\"stmp\":\"%s\",\"active\":\"0\"}}",EMCUTE_ID, stmp_str);
-        pub(MQTT_TOPIC_OUT, str, 0);
-    }
-
-}
-
-void make_sound(void){
-    gpio_set(alert_pin);
-    xtimer_usleep(200);
-    gpio_clear(alert_pin);
-    xtimer_usleep(200);
-}
-
-int getNextWakeTime(void){
-    return 3;
-}
-
-int main(void){
-    int distance=0;
-    int reference=0;
-    char str[40];
-    printf("Setting up MQTT-SN.\n");
-    setup_mqtt();
-    srf04_t dev;
-    srf04_params_t my_params;
-    my_params.trigger = trigger_pin;
-    my_params.echo = echo_pin;
-
-    if(srf04_init(&dev, &my_params) != SRF04_OK){
-        printf("Failed to setup SRF04");
-    }
-    gpio_init_int(operative_pin,GPIO_IN,GPIO_BOTH, &switch_cb, NULL);
-    gpio_init(led_pin,GPIO_OUT);
-    xtimer_sleep(3);
-    while(1) {
-        if(onTop){
-            printf("[MQTT] Publishing data to MQTT Broker\n");
-            distance=srf04_get_distance(&dev);
-            if(isReferenceMeasure){
-                reference=distance-margin; //The tank is considered full when there is some margin
-                isReferenceMeasure=0;
-            }
-            perc=(1-(distance/reference))*100;
-            if(perc>=0){
-                sprintf(str, "{\"id\":\"%s\",\"stmp\":\"%s\",\"perc\":\"%d\"}", EMCUTE_ID, stmp_str, perc);
-                pub(MQTT_TOPIC_OUT, str, 0);
-                if(perc>=100){
-                    make_sound();
-                    gpio_set(led_pin);
-                }
-            }
-            xtimer_sleep(getNextWakeTime());
-        }
-    }
-}
